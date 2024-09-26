0273febc4ba88376ffad5274e0fe9b5f56c004d4
A H Shuvro <ahshuvro77777@gmail.com>
2024-09-26 14:14:01 +0600

    change some designs

--- src/Components/Header/Header1.jsx
+++ src/Components/Header/Header1.jsx
@@ -8,7 +8,7 @@ const Header1 = () => {
         <div className="navbar bg-[#EF1E7E] h-[4.5rem] sticky top-0 z-10">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
+                    <div tabIndex={0} role="button" className="btn btn-ghost md:hidden text-white">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 duration-500 shadow bg-base-100 rounded-box w-52">
